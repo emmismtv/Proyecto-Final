c8ff02900d8ae8bfaa7769aaeb1f2067d4010e34
Sade-95e <omaremmtbb@gmail.com>
2024-05-02 10:46:13 -0800

    registrar_incidente

---
+++ proyecto.py
@@ -0,0 +1,17 @@
+#INCIDENTES
+
+
+import csv
+from datetime import datetime
+
+def registrar_incidente(filename):
+    with open(filename, mode='w', newline='') as cvsfile:
+        writer = csv.writer(cvsfile)
+        fecha_hora = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        descripcion = input("Escriba el incidente:")
+        gravedad= input("Escriba la gravedad (Alta, Media, Baja):")
+        area= ["Recursos humanos", "Auxiliares", "Administración"]
+        writer.writerow([fecha_hora, descripcion, gravedad])
+        print("Perfecto, el incidente ha sido registrado.")
+
+registrar_incidente('c:/Users/bumbl/OneDrive/Escritorio/Algoritmos/incidentes.csv')
